31528b27b19ea6ed80340c4add8f75fd134579ad
AANtoso <adjoaan@gmail.com>
2020-06-09 15:08:17 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
-          <h1>Quote Maker</h1>
-        </div>
-        <hr />
-        {/*
-          TODO:
+    render() {
+        return ( <
+            div className = "container-fluid" >
+            <
+            div className = "row title justify-content-center"
+            style = {
+                { paddingTop: '12px' } } >
+            <
+            h1 > Quote Maker < /h1> <
+            /div> <
+            hr / > {
+                /*
+                          TODO:
 
-          Render QuoteForm and Quotes Components
-         */}
-      </div>
-    );
-  }
+                          Render QuoteForm and Quotes Components
+                         */
+            } <
+            /div>
+        );
+    }
 }
 
 export default App;
+
+// arbitrary change
