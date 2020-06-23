67a5c5fc6dd8aa5e75eccf1c22c118d688b2d46d
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-06-23 16:47:18 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      // can't do this because id !== index
+      // return [...state.slice(0, action.quoteId), ...state.slice(action.quoteId + 1)];
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [...state.slice(0, index), { ...quote, votes: quote.votes += 1 }, ...state.slice(index + 1)];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      // do nothing if vote count is 0
+      if (quote.votes > 0) {
+        return [...state.slice(0, index), { ...quote, votes: quote.votes -= 1 }, ...state.slice(index + 1)]
+      }
+
+    default:
+      return state;
+  }
 }
