58f0457f03df68e03168f9a80ee4176ebf39eb83
kalmegati <kalmegati@gmail.com>
2020-06-24 17:03:21 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,36 @@
-export default (state = [], action) => {
-  return state;
+import uuid from 'uuid';
+ 
+const id = uuid();
+
+export default function quotes(state = [], action) {
+
+  switch(action.type) {
+
+    case 'ADD_QUOTE':
+      
+      let newState = state.concat({ 
+        id: id,
+        content: action.quote.content,
+        author: action.quote.author
+      });
+      console.log(newState)
+      return newState
+
+    case 'REMOVE_QUOTE':
+      console.log("REMOVE")
+      return null
+
+    case 'UPVOTE_QUOTE':
+      console.log("UPVOTE")
+      return null
+
+    case 'DOWNVOTE_QUOTE':
+      console.log("DOWNVOTE")
+      return null
+
+    default:
+      return state
+  }
+
 }
+
